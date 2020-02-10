c5a28013b401fbf7308e3282dfa64572358a2d51
hemanthkumarraju <41440715+hemanthkumarraju@users.noreply.github.com>
2020-02-10 19:43:35 +0530

    Add files via upload

---
+++ Coursera_Capstone week 1.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera_Capstone project_Week 1 #
+
+# ### Preparing Jupyter Notebook to upload in Github Repository
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print("Hello Capstone project Course")
+
+
+# In[ ]:
+
+
+
+
